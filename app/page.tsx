3cec22163cf5ed312af60b1e13c7fa3c07dd0a7f
kelvinpraises <kelvinpraises@gmail.com>
2024-06-18 23:21:37 +0100

    feat: add tabs

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,31 @@
 import GlassContainer from "@/components/molecules/glass-container";
+import {
+  Tabs,
+  TabsContent,
+  TabsList,
+  TabsTrigger,
+} from "@/components/molecules/tabs";
 import IntentsFeed from "@/components/organisms/intents-feed";
 import QueryIntents from "@/components/organisms/query-intents";
-import React from "react";
 
-const Home = () => {
+const IntentsHome = () => {
   return (
-    <div>
-      <GlassContainer>
-        <p className="p-4 font-semibold">Intents</p>
-        <div className="rounded-2xl bg-[#F8F8F7] p-4">
-          <IntentsFeed />
+    <GlassContainer>
+      <p className="pt-4 px-4 pb-2 font-semibold text-xl">Intents</p>
+      <Tabs defaultValue="query">
+        <TabsList className="grid w-full grid-cols-2 gap-2">
+          <TabsTrigger value="query">Query</TabsTrigger>
+          <TabsTrigger value="feed">Feed</TabsTrigger>
+        </TabsList>
+        <TabsContent className="rounded-2xl bg-[#F8F8F7] p-4" value="query">
           <QueryIntents />
-        </div>
-      </GlassContainer>
-    </div>
+        </TabsContent>
+        <TabsContent className="rounded-2xl bg-[#F8F8F7] p-4" value="feed">
+          <IntentsFeed />
+        </TabsContent>
+      </Tabs>
+    </GlassContainer>
   );
 };
 
-export default Home;
+export default IntentsHome;
