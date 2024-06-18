9c77fbc19b5c75913cf58a80f1e05d460ad11e67
kelvinpraises <kelvinpraises@gmail.com>
2024-06-18 18:20:15 +0100

    feat: add header

---
+++ library/components/organisms/header.tsx
@@ -0,0 +1,37 @@
+import { DynamicWidget } from "@dynamic-labs/sdk-react-core";
+
+import Tabs from "@/components/molecules/tabs";
+
+const tabs = [
+  {
+    title: "Intents",
+    value: "intent",
+  },
+  {
+    title: "My Swaps",
+    value: "swaps",
+  },
+  {
+    title: "Markets",
+    value: "markets",
+  },
+  {
+    title: "Docs",
+    value: "docs",
+  },
+];
+
+const Header = ({ className }: { className?: string }) => {
+  return (
+    <div
+      className={` flex justify-between items-center px-4 min-h-[70px] bg-transparent pr-8 ${className}`}
+    >
+      {/* <img src="/logo.svg" alt="" /> */}
+      <p className=" text-4xl">🔭</p>
+      <Tabs tabs={tabs} />
+      <DynamicWidget />
+    </div>
+  );
+};
+
+export default Header;
