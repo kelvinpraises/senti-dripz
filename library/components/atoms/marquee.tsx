8d050a086c4d15cef80644cb7e2e2f2af7a85e67
kelvinpraises <kelvinpraises@gmail.com>
2024-06-19 01:09:12 +0100

    chore: tidy up and update styles

--- library/components/atoms/marquee.tsx
+++ library/components/atoms/marquee.tsx
@@ -81,7 +81,7 @@ export const Marquee = ({
     <div
       ref={containerRef}
       className={cn(
-        "scroller relative z-20 w-full overflow-hidden [mask-image:linear-gradient(to_right,transparent,white_20%,white_80%,transparent)]",
+        "scroller relative z-20 w-full overflow-x-hidden [mask-image:linear-gradient(to_right,transparent,white_20%,white_80%,transparent)]",
         className
       )}
     >
