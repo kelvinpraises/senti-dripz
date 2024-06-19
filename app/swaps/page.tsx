9b4884131b7b0c1e7221371365b259dd4d634330
kelvinpraises <kelvinpraises@gmail.com>
2024-06-19 21:17:19 +0100

    feat: add pages

---
+++ app/swaps/page.tsx
@@ -0,0 +1,12 @@
+import GlassContainer from "@/components/molecules/glass-container";
+
+const SwapsHome = () => {
+  return (
+    <GlassContainer>
+      <p className="pt-4 px-4 pb-2 font-semibold text-xl">Swaps</p>
+      <div className="rounded-2xl bg-[#F8F8F7] p-4"></div>
+    </GlassContainer>
+  );
+};
+
+export default SwapsHome;
