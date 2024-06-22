05a0807cb2abecf5e24026bd9d910d7d32fb4e25
kelvinpraises <kelvinpraises@gmail.com>
2024-06-22 22:54:18 +0100

    feat: add query response

---
+++ library/components/organisms/query-response.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import IntentsFeed, { SwapIntent } from './intents-feed';
+
+type QueryResponseProps = {
+  text: string;
+  attachments: SwapIntent[];
+};
+
+const QueryResponse: React.FC<QueryResponseProps> = ({ text, attachments }) => {
+  return (
+    <div className=" w-full">
+      <p className="mb-4">{text}</p>
+      {attachments.length > 0 && (
+        <div>
+          <IntentsFeed swapIntents={attachments} showNewIntent={false} />
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default QueryResponse;
