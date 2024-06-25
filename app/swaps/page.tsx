218f758ccebd863c0b4cf8012cc53fdaf211a3d2
kelvinpraises <kelvinpraises@gmail.com>
2024-06-25 07:43:11 +0100

    chore: pretty print

--- app/swaps/page.tsx
+++ app/swaps/page.tsx
@@ -4,6 +4,7 @@ import { getSwapIntents } from "@/utils/db";
 
 const SwapsHome = async () => {
   const intents = await getSwapIntents({ limit: 25, offset: 0 });
+
   return (
     <GlassContainer>
       <p className="pt-4 px-4 pb-2 font-semibold text-xl">Swaps</p>
