aa5c75162c1343f3b7d88b1c5e81458c6e94b798
kelvinpraises <kelvinpraises@gmail.com>
2024-06-29 19:24:11 +0100

    chore: default to rounded-xl

--- library/components/atoms/card.tsx
+++ library/components/atoms/card.tsx
@@ -10,7 +10,7 @@ const Card = ({
   return (
     <div
       className={cn(
-        "flex flex-col gap-1 rounded-[20px] border-[1px] border-[#EFF1F8] p-4 shadow-[0_0_50px_7px_rgba(0,0,0,0.05)] bg-white",
+        "flex flex-col gap-1 rounded-xl border-[1px] border-[#EFF1F8] p-4 shadow-[0_0_50px_7px_rgba(0,0,0,0.05)] bg-white",
         className && className,
       )}
     >
