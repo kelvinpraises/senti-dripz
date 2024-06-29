4887fedc68635767beabe1638d326741fb5b2a9a
kelvinpraises <kelvinpraises@gmail.com>
2024-06-29 09:06:15 +0100

    chore: add pathname to track link change

--- library/components/molecules/nav-group.tsx
+++ library/components/molecules/nav-group.tsx
@@ -1,8 +1,9 @@
 "use client";
 
 import { motion } from "framer-motion";
-import { useState } from "react";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { useEffect, useState } from "react";
 
 import { cn } from "@/utils";
 
@@ -24,9 +25,17 @@ const NavGroup = ({
   activeNavClassName?: string;
   navClassName?: string;
 }) => {
+  const pathname = usePathname();
   const initialActiveNav = propNavs.find((nav) => nav.isActive) || propNavs[0];
   const [active, setActive] = useState<Nav>(initialActiveNav);
   const [navs, setNavs] = useState<Nav[]>(propNavs);
+
+  useEffect(() => {
+    const currentNav = propNavs.find((nav) => nav.href === pathname);
+    if (currentNav) {
+      setActive(currentNav);
+    }
+  }, [pathname, propNavs]);
 
   const moveSelectedNavToTop = (idx: number) => {
     const newNavs = [...propNavs];
@@ -40,7 +49,7 @@ const NavGroup = ({
     <div
       className={cn(
         "flex flex-row items-center justify-start [perspective:1000px] relative overflow-auto sm:overflow-visible no-visible-scrollbar max-w-full",
-        className,
+        className
       )}
     >
       {propNavs.map((nav, idx) => (
@@ -61,15 +70,14 @@ const NavGroup = ({
               transition={{ type: "spring", bounce: 0.3, duration: 0.6 }}
               className={cn(
                 "absolute inset-0 bg-gray-200 dark:bg-zinc-800 rounded-full ",
-                activeNavClassName,
+                activeNavClassName
               )}
             />
           )}
-
           <span
             className={cn(
               "relative block text-black text-nowrap",
-              active.value === nav.value && "text-white",
+              active.value === nav.value && "text-white"
             )}
           >
             {nav.title}
